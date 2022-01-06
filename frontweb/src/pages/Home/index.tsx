c528a99e70d547583d4ffbd2137a0ba0fd75eac6
Henrique Souza <henrique.asouza@outlook.com>
2022-01-05 23:17:48 -0300

    fix: prevent authenticated user to access login page

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg'
 import Login from 'components/Login'
+import { useHistory, useLocation } from 'react-router-dom'
+import { isAuthenticated } from 'utils/auth'
 import './styles.css'
+
+type LocationState = {
+  from: string
+}
+
 const Home = () => {
+  const history = useHistory()
+  const location = useLocation<LocationState>()
+  const { from } = location.state || { from: { pathname: '/movies' } }
+  if (isAuthenticated()) {
+    history.push(from)
+  }
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
